d015df130a80b7a1f474dd48f0b512c0c0496630
Mufakkharul Islam Nayem <mufakkharul.nayem@bjitgroup.com>
2019-02-19 12:04:46 +0600

    Initial Commit

---
+++ PopupTableViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PopupTableViewDemo
+//
+//  Created by Nayem BJIT on 2/19/19.
+//  Copyright © 2019 BJIT Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
